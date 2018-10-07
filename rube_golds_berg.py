56d5479ce93bb8d121cd3f1121792ad9fbfe2559
BallisticZer0 <37849667+BallisticZer0@users.noreply.github.com>
2018-10-07 17:48:03 -0600

    Add files via upload

---
+++ rube_golds_berg.py
@@ -0,0 +1,32 @@
+import random
+import time
+
+i = 3
+def Hack():
+    print("The number is",random.randint(1,10))
+    time.sleep(1)
+    
+    if  Hack == 1:
+        print("Hello world")       
+        time.sleep(2,quit)
+        
+    elif Hack == 2:
+        print("wrong number ill try again.")
+        Hack()
+
+
+   
+
+while i == 3:
+    Hack()
+    
+  
+
+
+time.sleep(1)
+Hack = input("")
+Hack = int(Hack)
+
+
+        
+
